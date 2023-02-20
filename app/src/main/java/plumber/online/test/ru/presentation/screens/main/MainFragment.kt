1e0ae207ca1d577c43d0a25ab0273544cadc2bb1
daniel <meh.daniel@yandex.com>
2023-02-20 15:47:45 +0700

    создание MainF экрана

--- app/src/main/java/plumber/online/test/ru/presentation/screens/main/MainFragment.kt
+++ app/src/main/java/plumber/online/test/ru/presentation/screens/main/MainFragment.kt
@@ -1,13 +1,21 @@
 package plumber.online.test.ru.presentation.screens.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.viewModels
+import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.onEach
 import plumber.online.test.ru.R
 import plumber.online.test.ru.databinding.MainFragmentBinding
 import plumber.online.test.ru.presentation.base.BaseFragment
+import plumber.online.test.ru.presentation.utils.Utils
 import plumber.online.test.ru.presentation.utils.observeInLifecycle
 
 @AndroidEntryPoint
@@ -26,9 +34,9 @@ class MainFragment: BaseFragment<MainViewModel, MainFragmentBinding>(R.layout.ma
     }
 
     private fun setupStateFlow() {
-        viewModel.stateFlow.onEach {
+        viewModel.stateFlow.onEach { state ->
             with(binding){
-
+                infoLocationTxt.text = if(state is MainState.Loaded) state.data else ""
             }
         }.observeInLifecycle(viewLifecycleOwner)
     }
@@ -41,6 +49,15 @@ class MainFragment: BaseFragment<MainViewModel, MainFragmentBinding>(R.layout.ma
         }.observeInLifecycle(viewLifecycleOwner)
     }
 
-
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun setupListeners() {
+        binding.showOnMapBtn.setOnClickListener {
+            if (Utils.isNetworkAvailable(this@MainFragment.requireContext())) {
+                viewModel.sendAction(MainAction.RouteToMap)
+            } else {
+                viewModel.sendAction(MainAction.ShowError("Отсутствие интернета"))
+            }
+        }
+    }
 
 }
--- app/src/main/java/plumber/online/test/ru/presentation/screens/main/MainViewModel.kt
+++ app/src/main/java/plumber/online/test/ru/presentation/screens/main/MainViewModel.kt
@@ -1,20 +1,64 @@
 package plumber.online.test.ru.presentation.screens.main
 
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import plumber.online.test.ru.domain.MapRepository
 import plumber.online.test.ru.presentation.base.BaseViewModel
+import plumber.online.test.ru.presentation.utils.Utils
+import java.net.ConnectException
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): BaseViewModel() {
+class MainViewModel @Inject constructor(
+    private val repository: MapRepository
+): BaseViewModel() {
+
+    private val _state = MutableStateFlow<MainState>(MainState.Idle)
+    var stateFlow = _state.asStateFlow()
 
     private val _action: Channel<MainAction> = Channel(Channel.BUFFERED)
     var actionFlow = _action.receiveAsFlow()
 
-    private val _state = MutableStateFlow<MainState>(MainState.Loading)
-    var stateFlow = _state.asStateFlow()
+    init {
+        load()
+    }
+
+    private fun load() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                repository.get()
+            } catch (e: Throwable) {
+                when (e) {
+                    is NullPointerException -> setState(MainState.Loaded("Точка не задана"))
+                    is ConnectException -> setState(MainState.Error("отсутствует интернет"))
+                    else -> setState(MainState.Error(e.message.toString()))
+                }
+            }
+        }
+    }
+
+    fun goToMap() {
+        viewModelScope.launch(Dispatchers.IO) {
+
+        }
+    }
+
+    private fun setState(state: MainState) {
+        viewModelScope.launch(Dispatchers.Main) {
+            _state.value = state
+        }
+    }
+
+    fun sendAction(action: MainAction){
+        viewModelScope.launch(Dispatchers.Main){
+            _action.send(action)
+        }
+    }
 
 }

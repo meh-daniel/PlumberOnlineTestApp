0cdcd267ca7b48cd1ddd22a8bb9a856ea8517819
daniel <meh.daniel@yandex.com>
2023-02-20 15:47:10 +0700

    исправил неправильный Hilt Module

--- app/src/main/java/plumber/online/test/ru/di/MapComponentModule.kt
+++ app/src/main/java/plumber/online/test/ru/di/MapComponentModule.kt
@@ -1,13 +1,18 @@
 package plumber.online.test.ru.di
 
 import android.content.Context
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import plumber.online.test.ru.data.MapRepositoryImpl
 import plumber.online.test.ru.data.sw.MapDataBase
 import plumber.online.test.ru.domain.MapRepository
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 class MapComponentModule {
 
     @Provides

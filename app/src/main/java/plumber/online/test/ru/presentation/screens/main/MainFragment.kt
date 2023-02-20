1c8bc96673f0d17faddf3e6c75bf6064a827d47b
daniel <meh.daniel@yandex.com>
2023-02-20 11:44:10 +0700

    Создание главного экрана выбора и его viewModel

---
+++ app/src/main/java/plumber/online/test/ru/presentation/screens/main/MainFragment.kt
@@ -0,0 +1,18 @@
+package plumber.online.test.ru.presentation.screens.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import plumber.online.test.ru.R
+import plumber.online.test.ru.databinding.MainFragmentBinding
+import plumber.online.test.ru.presentation.base.BaseFragment
+
+class MainFragment: BaseFragment<MainViewModel, MainFragmentBinding>(R.layout.main_fragment) {
+
+    override val viewModel: MainViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun initBinding(inflater: LayoutInflater, container: ViewGroup?): MainFragmentBinding {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/plumber/online/test/ru/presentation/screens/main/MainViewModel.kt
@@ -0,0 +1,5 @@
+package plumber.online.test.ru.presentation.screens.main
+
+import plumber.online.test.ru.presentation.base.BaseViewModel
+
+class MainViewModel: BaseViewModel()
